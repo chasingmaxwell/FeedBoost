c8974e5dc4cbfcc98860c414812f65042f22757e
Peter Sieg <chasingmaxwell@gmail.com>
2016-12-10 00:54:21 -0500

    Add styling for pages.

--- src/handlers/app/home.js
+++ src/handlers/app/home.js
@@ -26,7 +26,7 @@ module.exports = (event, context, callback) => {
     page.content = `
       <h2>Hi, ${user.email}!</h2>
       <p>You've subscribed to receive email notifications when new items appear in your feed.</p>
-      <a href="${process.env.REVERB_HOST}/my/feed">Take me to my feed!</a>
+      <p class="cta"><a href="${process.env.REVERB_HOST}/my/feed">Take me to my feed!</a></p>
     `;
     page.footer = `
       <p>Would you like to stop receiving notifications? <a href="${process.env.BASE_URI}/unsubscribe">Unsubscribe</a>.</p>
@@ -38,7 +38,7 @@ module.exports = (event, context, callback) => {
     page.content = `
       <h2>Ditch the delay in your Reverb.com feed!</h2>
       <p>Ever missed out on some rockin' gear because you didn't receive the feed notification in time? That gear gets snatched up quick! ${process.env.APP_NAME} boosts the signal of your Reverb.com feed by ditching the delay and notifying you of changes to your feed within one hour.</p>
-      <p><a href="${process.env.BASE_URI}/subscribe">Boost my feed!</a></p>
+      <p class="cta"><a href="${process.env.BASE_URI}/subscribe">Boost my feed!</a></p>
     `;
     page.footer = `
       <p>Already subscribed? <a href="${process.env.BASE_URI}/login">Log in</a> to manage your preferences.</p>
@@ -58,6 +58,115 @@ module.exports = (event, context, callback) => {
       <html>
       <head>
         <title>${process.env.APP_NAME}</title>
+        <meta name="viewport" content="width=device-width" />
+        <link href="https://fonts.googleapis.com/css?family=Yesteryear" rel="stylesheet">
+        <style type="text/css">
+          /* http://meyerweb.com/eric/tools/css/reset/
+             v2.0 | 20110126
+             License: none (public domain)
+          */
+          html, body, div, span, applet, object, iframe,
+          h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+          a, abbr, acronym, address, big, cite, code,
+          del, dfn, em, img, ins, kbd, q, s, samp,
+          small, strike, strong, sub, sup, tt, var,
+          b, u, i, center,
+          dl, dt, dd, ol, ul, li,
+          fieldset, form, label, legend,
+          table, caption, tbody, tfoot, thead, tr, th, td,
+          article, aside, canvas, details, embed,
+          figure, figcaption, footer, header, hgroup,
+          menu, nav, output, ruby, section, summary,
+          time, mark, audio, video {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-size: 100%;
+            font: inherit;
+            vertical-align: baseline;
+          }
+          /* HTML5 display-role reset for older browsers */
+          article, aside, details, figcaption, figure,
+          footer, header, hgroup, menu, nav, section {
+            display: block;
+          }
+          body {
+            line-height: 1;
+          }
+          ol, ul {
+            list-style: none;
+          }
+          blockquote, q {
+            quotes: none;
+          }
+          blockquote:before, blockquote:after,
+          q:before, q:after {
+            content: '';
+            content: none;
+          }
+          table {
+            border-collapse: collapse;
+            border-spacing: 0;
+          }
+
+          /* Custom styles */
+          html, body {
+            background: #f5f5f5;
+          }
+          body {
+            font-family: "PT Sans", "Verdana", sans-serif;
+            color: #333;
+            line-height: 1.75em;
+            max-width: 560px;
+            margin: 0 auto;
+            padding: 0 10px;
+          }
+          h2, h3, h4, h5, h6 {
+            font-weight: bold;
+            line-height: 1.5em;
+            margin: .5em 0;
+          }
+          h2 {
+            font-size: 1.5em;
+          }
+          a {
+            color: #0080a5;
+            text-decoration: none;
+          }
+          a:hover, a:visited, a:active {
+            color: #00607d;
+          }
+          #logo {
+            font-size: 50px;
+            font-weight: 400px;
+            font-family: 'Yesteryear', cursive;
+            line-height: 2;
+            text-align: center;
+          }
+          .cta {
+            text-align: center;
+            margin: 2.5em 0;
+          }
+          .cta a {
+            display: block;
+            background: #0080a5;
+            padding: .5em .75em;
+            color: #fff;
+            font-size: 20px;
+            text-shadow: #00495f 0 1px;
+            border-radius: 2px;
+          }
+          .cta a:hover {
+            background: #00607d;
+            text-shadow: #333;
+          }
+          #page-footer {
+            position: relative;
+            margin-top: 3em;
+            padding: 1em 0;
+            color: #999;
+          }
+        </style>
       </head>
       <body>
         <header id="page-header">

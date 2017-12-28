f45d6e2f93241126d9312c634fa32ece9420860a
Peter Sieg <chasingmaxwell@gmail.com>
2017-12-27 22:41:22 -0500

    fix(initiation): put default config back in place

---
+++ config/default.js
@@ -0,0 +1,17 @@
+module.exports = {
+  app: {
+    name: process.env.APP_NAME,
+    email: process.env.APP_EMAIL,
+    filesUrl: process.env.FILES_URL,
+    baseUri: process.env.BASE_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    cryptrKey: process.env.CRYPTR_KEY,
+    allowedUsers: process.env.ALLOWED_USERS,
+  },
+  reverb: {
+    host: process.env.REVERB_HOST,
+    key: process.env.REVERB_KEY,
+    secret: process.env.REVERB_SECRET,
+    redirectPath: process.env.REVERB_REDIRECT_PATH,
+  },
+};

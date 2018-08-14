b67910a06f4d1e34c939af7decb9536d6ed31d0c
Peter Sieg <chasingmaxwell@gmail.com>
2018-08-14 18:11:41 -0400

    fix: catch errors when retrieving the token (#19)

--- src/handlers/app/home.js
+++ src/handlers/app/home.js
@@ -38,7 +38,10 @@ const handler: LambdaHandler = (event, context, callback) => {
 
   // Get the token.
   return (
-    Promise.resolve(Token.getFromCookie(cookieString))
+    Promise.resolve()
+
+      // Try to get the token from the cookie.
+      .then(() => Token.getFromCookie(cookieString))
 
       // Get the user.
       .then(token => User.getFromToken(token))
